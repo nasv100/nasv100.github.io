b0ecde9fb4d58fcb29c1246d117bae8d88a9cda4
Babkin Evgeny <babkinevgeny@gmail.com>
2016-02-01 15:56:09 +0300

    add popup

--- js/main.js
+++ js/main.js
@@ -36,6 +36,49 @@ $(document).ready(function() {
 		}
 	});
 
+	$('.btn--photo').magnificPopup({
+    items: [
+      {
+        src: 'img/pic/1.jpg'
+      },
+      {
+        src: 'img/pic/2.jpg'
+      },
+      {
+        src: 'img/pic/3.jpg'
+      },
+      {
+        src: 'img/pic/4.jpg'
+      },
+      {
+        src: 'img/pic/5.jpg'
+      },
+      {
+        src: 'img/pic/6.jpg'
+      },
+      {
+        src: 'img/pic/7.jpg'
+      },
+      {
+        src: 'img/pic/8.jpg'
+      },
+      {
+        src: 'img/pic/9.jpg'
+      },
+      {
+        src: 'img/pic/10.jpg'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image',
+    image: {
+    cursor: "pointer"
+    }
+    
+	});
+
 	$(".menu ul a").mPageScroll2id();
 });
 
